53e70be55032cb0b47916ce45bb1b06e88a9b1c8
Martin Gleich <martin.gleich3@web.de>
2020-09-01 19:10:42 +0200

    Add int arguments

--- CommandLineParser/CommandLineParser.cs
+++ CommandLineParser/CommandLineParser.cs
@@ -32,9 +32,12 @@ namespace CmdParse
 
 	public class CommandLineConfiguration
 	{
-		public CommandLineConfiguration(ImmutableArray<Option> booleanArguments)
+		public CommandLineConfiguration(
+			ImmutableArray<Option> booleanArguments,
+			ImmutableArray<IntegerArgument> integerArguments)
 		{
 			Options = booleanArguments;
+			IntegerArguments = integerArguments;
 		}
 
 		public class Option
@@ -51,14 +54,27 @@ namespace CmdParse
 			public string Name { get;}
 			public string LongName => "--" + Name;
 		}
+		public class IntegerArgument
+		{
+			public IntegerArgument(FieldInfo location, string name)
+			{
+				Location = location;
+				Name = name;
+			}
+			public FieldInfo Location { get; }
+			public string Name { get; }
+			public string LongName => "--" + Name;
+		}
 	
 		public ImmutableArray<Option> Options { get; }
+		public ImmutableArray<IntegerArgument> IntegerArguments { get; }
 		
 		public static CommandLineConfiguration Create(Type t)
 		{
 			// 1) Scan public writable values of t.
 			var fields = t.GetFields(BindingFlags.Public | BindingFlags.Instance);
 			List<Option> options = new List<Option>();
+			List<IntegerArgument> intArgs = new List<IntegerArgument>();
 			foreach (var field in fields)
 			{
 				if (field.FieldType == typeof(bool))
@@ -67,31 +83,62 @@ namespace CmdParse
 					var defaultValue = field.GetCustomAttribute<CmdOptionDefaultAttribute>()?.DefaultValue ?? false;
 					options.Add(new Option(field, name, defaultValue));
 				}
+				else if (field.FieldType == typeof(int))
+				{
+					var name = field.Name;
+					intArgs.Add(new IntegerArgument(field, name));
+				}
 			}
 
-			return new CommandLineConfiguration(options.ToImmutableArray());
+			return new CommandLineConfiguration(options.ToImmutableArray(), intArgs.ToImmutableArray());
 		}
 
 		public ErrorOr<T> Parse<T>(string[] args)
 		{
 			Dictionary<Option, bool> values = new Dictionary<Option, bool>();
-			foreach (var arg in args)
+			Dictionary<IntegerArgument, int> intValues = new Dictionary<IntegerArgument, int>();
+			for(int i =0; i< args.Length; ++i)
 			{
-				var matched = Options.Where(b => b.LongName == arg).FirstOrDefault();
-				if (matched == null)
+				var arg = args[i];
+				var matchedOption = Options.Where(b => b.LongName == arg).FirstOrDefault();
+				if (matchedOption != null)
+				{
+					if (!values.TryAdd(matchedOption, !matchedOption.DefaultValue))
+						return $"Duplicate option '{arg}'.";
+					continue;
+				}
+				var matchedIntArg = IntegerArguments.Where(b => b.LongName == arg).FirstOrDefault();
+				if (matchedIntArg != null)
+				{
+					var valueArg = i + 1 < args.Length ? args[i + 1] : null;
+					if (!int.TryParse(valueArg, out int value))
+						return $"Missing the value for '{arg}'.";
+					if (!intValues.TryAdd(matchedIntArg, value))
+						return $"Duplicate option '{arg}'.";
+					++i;
+					continue;
+				}
+				else
+				{
 					return $"Unknown option '{arg}'.";
-				if (!values.TryAdd(matched, !matched.DefaultValue))
-					return $"Duplicate option '{arg}'.";
+				}
 			}
 			foreach (var arg in Options)
 			{
 				if (!values.ContainsKey(arg))
 					values.Add(arg, arg.DefaultValue);
 			}
+			foreach (var arg in IntegerArguments)
+			{
+				if(!intValues.ContainsKey(arg))
+					return $"Missing mandatory argument '{arg.LongName}'.";
+			}
 
 			var result = (T)Activator.CreateInstance(typeof(T));
 			foreach(var opt in Options)
 				opt.Location.SetValue(result, values[opt]);
+			foreach(var arg in IntegerArguments)
+				arg.Location.SetValue(result, intValues[arg]);
 			return ErrorOr.FromValue(result);
 		}
 	}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -66,5 +66,39 @@ namespace Tests
 			Assert.True(result.Value2);
 			Assert.False(result.Value3);
 		}
+		public class IntTestType1
+		{
+			public int Value1;
+		}
+		[Fact]
+		public void IntParse()
+		{
+			var result = CommandLineParser.Parse<IntTestType1>(new[] { "--Value1", "435" });
+			Assert.Equal(435, result.Value1);
+		}
+		[Fact]
+		public void IntParse_MissingMandatory()
+		{
+			var result = CommandLineParser.ParseWithError<IntTestType1>(new string[] { });
+			Assert.False(result.IsOkay);
+		}
+		[Fact]
+		public void IntParse_MissingValue()
+		{
+			var result = CommandLineParser.ParseWithError<IntTestType1>(new [] {"--Value1" });
+			Assert.False(result.IsOkay);
+		}
+		[Fact]
+		public void IntParse_MissingValueNoInt()
+		{
+			var result = CommandLineParser.ParseWithError<IntTestType1>(new [] {"--Value1", "Hi" });
+			Assert.False(result.IsOkay);
+		}
+		[Fact]
+		public void IntParse_Duplicate()
+		{
+			var result = CommandLineParser.ParseWithError<IntTestType1>(new [] {"--Value1", "123", "--Value1", "456" });
+			Assert.False(result.IsOkay);
+		}
 	}
 }
