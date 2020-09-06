d00c088078c10e0c8376978d18b48a65a51ae59b
Martin Gleich <martin.gleich3@web.de>
2020-09-06 10:58:32 +0200

    Delete OptionSettings file.

--- CommandLineParser/AritySettings.cs
+++ CommandLineParser/AritySettings.cs
@@ -1,26 +1,5 @@
 ﻿namespace CmdParse
 {
-	public sealed class OptionalSettings
-	{
-		public static readonly OptionalSettings Excepted = new OptionalSettings(false, null);
-		public static OptionalSettings Optional(object? defaultValue)
-			=> new OptionalSettings(true, defaultValue);
-		private OptionalSettings(bool isOptional, object? defaultValue)
-		{
-			IsOptional = isOptional;
-			DefaultValue = defaultValue;
-		}
-
-		public bool IsOptional { get; }
-		private object? DefaultValue { get; }
-
-		public bool GetDefaultValue(out object? value)
-		{
-			value = DefaultValue;
-			return IsOptional;
-		}
-	}
-
 	public class AritySettings
 	{
 		public static AritySettings Expected => new AritySettings(Arity.One, default);
