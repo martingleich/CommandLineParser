d464bf60563d52b5a56acf8c5dc0e5b79fff1030
Martin Gleich <martin.gleich3@web.de>
2020-09-14 18:27:53 +0200

    Move help tests to seperate file

---
+++ Tests/HelpTests.cs
@@ -0,0 +1,34 @@
+﻿using CmdParse;
+using Xunit;
+
+namespace Tests
+{
+	public class HelpTests
+	{
+		[CmdProgramDescription("MyProgram", Description = @"A cool program todo things.
+With multiline.")]
+		class BasicType
+		{
+			[CmdArgumentDescription(@"A argument.
+Another line of comments.")]
+			public int myArgument;
+			[CmdArgumentDescription("A option.")]
+			public bool myOption;
+		}
+
+		[Fact]
+		public void ShowBasicHelp()
+		{
+			var config = new CommandLineConfigurationFactory().Create<BasicType>();
+			var help = new HelpPrinter().PrintHelp(config);
+			Assert.Equal(@"A cool program todo things.
+With multiline.
+
+MyProgram --myArgument <Int32>
+
+  --myArgument : Int32  A argument.
+                        Another line of comments.
+  --myOption            A option.", help);
+		}
+	}
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -63,33 +63,4 @@ namespace Tests
 			Assert.DoesNotContain("IgnoredValue", config.Arguments.Select(a => a.Name));
 		}
 	}
-
-	public class HelpTests
-	{
-		[CmdProgramDescription("MyProgram", Description = @"A cool program todo things.
-With multiline.")]
-		class BasicType
-		{
-			[CmdArgumentDescription(@"A argument.
-Another line of comments.")]
-			public int myArgument;
-			[CmdArgumentDescription("A option.")]
-			public bool myOption;
-		}
-
-		[Fact]
-		public void ShowBasicHelp()
-		{
-			var config = new CommandLineConfigurationFactory().Create<BasicType>();
-			var help = new HelpPrinter().PrintHelp(config);
-			Assert.Equal(@"A cool program todo things.
-With multiline.
-
-MyProgram --myArgument <Int32>
-
-  --myArgument : Int32  A argument.
-                        Another line of comments.
-  --myOption            A option.", help);
-		}
-	}
 }
